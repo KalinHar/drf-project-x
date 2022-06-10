dbbd04cdf385c36868d8ab7f36e8091aad404722
KalinHar <kalix@abv.bg>
2022-06-10 15:35:00 +0300

    add convert lead to client

--- client/views.py
+++ client/views.py
@@ -1,10 +1,16 @@
+from rest_framework import viewsets, status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from django.http import Http404
 from django.shortcuts import render
 from django.contrib.auth.models import User
-from requests import request
-from rest_framework import viewsets
+from requests import Request
 from .models import Client, Note
 from .serializers import ClientSerializer, NoteSerializer
 from team.models import Team
+from lead.models import Lead
 
 class ClientViewSet(viewsets.ModelViewSet):
     serializer_class = ClientSerializer
@@ -34,3 +40,17 @@ class NoteViewSet(viewsets.ModelViewSet):
         client_id = self.request.data['client_id']
         
         serializer.save(team=team, created_by=self.request.user, client_id=client_id)
+
+@api_view(['POST'])
+def convert_lead_to_client(request):
+    team = Team.objects.filter(members__in=[request.user]).first()
+    lead_id = request.data['lead_id']
+
+    try:
+        lead = Lead.objects.filter(team=team).get(pk=lead_id)
+    except Lead.DoesNotExist:
+        raise Http404
+    
+    client = Client.objects.create(team=team, name=lead.company, contact_person=lead.contact_person, email=lead.email, phone=lead.phone, website=lead.website, created_by=request.user)
+
+    return Response()
