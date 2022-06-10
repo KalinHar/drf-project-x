1d86157308109d76001bead6b7704f54712bc145
KalinHar <kalix@abv.bg>
2022-06-10 12:54:46 +0300

    add model node to client

--- client/views.py
+++ client/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
+from requests import request
 from rest_framework import viewsets
-from .models import Client
-from .serializers import ClientSerializer
+from .models import Client, Note
+from .serializers import ClientSerializer, NoteSerializer
 from team.models import Team
 
 class ClientViewSet(viewsets.ModelViewSet):
@@ -18,4 +19,18 @@ class ClientViewSet(viewsets.ModelViewSet):
         
         serializer.save(team=team, created_by=self.request.user)
 
-            
+class NoteViewSet(viewsets.ModelViewSet):
+    serializer_class = NoteSerializer
+    queryset = Note.objects.all()
+
+    def get_queryset(self):
+        team = Team.objects.filter(members__in=[self.request.user]).first()
+        client_id = request.Get.get('client_id')
+
+        return self.queryset.filter(team=team).filter(client_id=client_id)
+
+    def perform_create(self, serializer):
+        team = Team.objects.filter(members__in=[self.request.user]).first()
+        client_id = request.data['client_id']
+        
+        serializer.save(team=team, created_by=self.request.user, client_id=client_id)
